7a730f8646575b266d9940e4aac2d3a839cfa6ec
Roman Mokrushin <emptyspaceman1@gmail.com>
2023-01-22 00:14:03 +0200

    adds finished task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,71 +1,72 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { Box } from '../Box';
 import { FeedbackOptions, Notification, Statistics, Title } from 'components';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, bad, neutral } = this.state;
+  const countTotalFeedback = () => {
     return good + bad + neutral;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good, bad, neutral } = this.state;
+  const countPositiveFeedbackPercentage = () => {
     const number = (100 * good) / (bad + good + neutral);
     return good ? `${Math.round(number)}%` : 0;
   };
 
-  onLeaveFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
+  const onLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        return new Error(`Unknown option - ${option}`);
+    }
   };
 
-  render() {
-    const options = Object.keys(this.state);
-    const { bad, good, neutral } = this.state;
+  const options = ['good', 'neutral', 'bad'];
 
-    return (
-      <Box
-        mx="auto"
-        my="100px"
-        p={3}
-        width="500px"
-        border="1px solid rgba(0, 0, 0, 0.2)"
-        borderRadius="5px"
-        boxShadow="0 3px 7px rgba(0, 0, 0, 0.3)"
-      >
-        <Box>
-          <Title text="please leave feedback" />
+  return (
+    <Box
+      mx="auto"
+      my="100px"
+      p={3}
+      width="500px"
+      border="1px solid rgba(0, 0, 0, 0.2)"
+      borderRadius="5px"
+      boxShadow="0 3px 7px rgba(0, 0, 0, 0.3)"
+    >
+      <Box>
+        <Title text="please leave feedback" />
 
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Box>
-
-        <Box mt={3}>
-          <Title text="statistics" />
-
-          {good || neutral || bad ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="there is no feedback." />
-          )}
-        </Box>
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
       </Box>
-    );
-  }
-}
+
+      <Box mt={3}>
+        <Title text="statistics" />
+
+        {good || neutral || bad ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="there is no feedback." />
+        )}
+      </Box>
+    </Box>
+  );
+};
